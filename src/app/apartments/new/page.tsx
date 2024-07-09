8618eb6018aae74b4155e81c6693cb7f26fb9ac1
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-10 00:01:56 +0200

    Add form with input elements

---
+++ src/app/apartments/new/page.tsx
@@ -0,0 +1,20 @@
+export default function NewApartment() {
+  return (
+    <main>
+      <h1>New apartment</h1>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="location">Location</label>
+        <input type="text" id="location" name="location" />
+        <label htmlFor="price">Price</label>
+        <input type="number" id="price" name="price" />
+        <label htmlFor="rooms">Rooms</label>
+        <input type="number" id="rooms" name="rooms" />
+        <label htmlFor="imageURL">Image URL</label>
+        <input type="text" id="imageURL" name="imageURL" />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  )
+}
