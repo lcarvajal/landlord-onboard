a4f0bfbc2cae8bdb50ffb8a7dbe29a6352256616
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-11 19:19:11 +0200

    Add Bottombar component for navigating through apartment flow

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 
-export default function Navbar() {
+export function Navbar() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const handleDrawerToggle = () => {
@@ -18,4 +18,24 @@ export default function Navbar() {
       </div>
     </div>
   );
+};
+
+interface BottombarProps {
+  formId: string,
+  buttonAction: (formData: FormData) => void,
+  buttonTitle: string
+}
+
+export function Bottombar(props: BottombarProps) {
+  return (
+    <div className="w-full bg-white px-6 py-4 flex flex-col justify-center absolute bottom-0">
+      <div className="flex ml-auto">
+        <Link href="/">
+          <button className="primary-button" form={props.formId} formAction={props.buttonAction}>
+            {props.buttonTitle}
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 };
