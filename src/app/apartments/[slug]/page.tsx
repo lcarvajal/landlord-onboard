e5421cfc2f2ea35d36da1e6c2890b8041cca1857
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-11 22:44:18 +0200

    Clean up copy for title and sub-title

--- src/app/apartments/[slug]/page.tsx
+++ src/app/apartments/[slug]/page.tsx
@@ -6,63 +6,64 @@ export default async function Apartment({ params }: { params: { slug: string } }
   const apartment = await getApartment(params.slug);
 
   return (
-    <main className="h-screen flex flex-col items-center p-12">
-      <div className="text-center mb-6">
-        <h1 className="text-3xl">{apartment.name}</h1>
-        <h2 className="text-xl">Rooms available at {apartment.location}</h2>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        {
-          apartment.rooms.map((room, index) => (
-            <div key={room.id}>
-              <p>Room {index + 1}</p>
-              <form id={`room-${room.id}`}>
-                {room.image_url && <Image className="max-h-24 object-scale-down" src={`https://utmxzqgmamtmrrxbygqb.supabase.co/storage/v1/object/public/${room.image_url}`} alt={room.name ? room.name : "image of room"} width={200} height={200} />}
-                <input
-                  type="hidden"
-                  name="apartment_id"
-                  value={apartment.id}
-                />
-                <input
-                  type="hidden"
-                  name="room_id"
-                  value={room.id}
-                />
-                <label htmlFor="name">Room name:</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={room.name as string}
-                  required
-                />
-                <label htmlFor="size">Size in square meters:</label>
-                <input
-                  type="number"
-                  id="size"
-                  name="size"
-                  value={room.size as number}
-                  min={1}
-                  required
-                />
-                <label htmlFor="equipment">Equipment:</label>
-                <input
-                  className="mb-6"
-                  type="text"
-                  id="equipment"
-                  name="equipment"
-                  value={room.equipment as string}
-                  required
-                />
-                <label htmlFor="image">Image:</label>
-                <input type="file" name="image" id="image" accept="image/*" />
-                <button className="primary-button" formAction={updateRoom}>Update</button>
-              </form>
-            </div>
-          ))
-        }
-      </div>
-      <Bottombar formId="n/a" buttonTitle="Complete" buttonAction={navigateHome} />
-    </main>
+    <>
+      <main className="h-screen flex flex-col items-center p-12">
+        <div className="text-center mb-6">
+          <h1 className="text-3xl">{apartment.name}</h1>
+          <h2 className="text-xl">{apartment.location}</h2>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {
+            apartment.rooms.map((room, index) => (
+              <div className="flex flex-col items-center" key={room.id}>
+                <p className="w-full text-left">Room {index + 1}</p>
+                <form id={`room-${room.id}`}>
+                  {room.image_url && <Image className="max-h-24 object-scale-down" src={`https://utmxzqgmamtmrrxbygqb.supabase.co/storage/v1/object/public/${room.image_url}`} alt={room.name ? room.name : "image of room"} width={200} height={200} />}
+                  <input
+                    type="hidden"
+                    name="apartment_id"
+                    value={apartment.id}
+                  />
+                  <input
+                    type="hidden"
+                    name="room_id"
+                    value={room.id}
+                  />
+                  <label htmlFor="name">Room name:</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={room.name as string}
+                    required
+                  />
+                  <label htmlFor="size">Size in square meters:</label>
+                  <input
+                    type="number"
+                    id="size"
+                    name="size"
+                    value={room.size as number}
+                    min={1}
+                    required
+                  />
+                  <label htmlFor="equipment">Equipment:</label>
+                  <input
+                    type="text"
+                    id="equipment"
+                    name="equipment"
+                    value={room.equipment as string}
+                    required
+                  />
+                  <label htmlFor="image">Image:</label>
+                  <input className="mb-6" type="file" name="image" id="image" accept="image/*" />
+                  <button className="primary-button" formAction={updateRoom}>Update</button>
+                </form>
+              </div>
+            ))
+          }
+        </div>
+      </main>
+      <Bottombar formId="n/a" buttonTitle="Done" buttonAction={navigateHome} />
+    </>
   )
 }
