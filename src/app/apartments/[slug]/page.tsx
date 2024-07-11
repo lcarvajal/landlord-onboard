bb62f43eb5fa234a34bb1b19ed6ebca087bf50c0
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-11 02:41:43 +0200

    Display apartment info and rooms with forms to edit room metadata

--- src/app/apartments/[slug]/page.tsx
+++ src/app/apartments/[slug]/page.tsx
@@ -13,7 +13,8 @@ export default async function Apartment({ params }: { params: { slug: string } }
       location,
       price,
       currency,
-      description
+      description,
+      rooms: rooms(*)
         `
     )
     .eq('id', params.slug)
@@ -23,14 +24,57 @@ export default async function Apartment({ params }: { params: { slug: string } }
 
   const { data, error } = await apartmentQuery;
   if (error) throw error;
-  console.log(data);
   const apartment: Apartment = data;
 
   return (
-    <main>
-      <h1>{params.slug}</h1>
-      <p>{apartment.name}</p>
-      <p>{apartment.price}</p>
+    <main className="h-screen flex flex-col items-center p-12">
+      <div className="text-center mb-6">
+        <h1 className="text-3xl">{apartment.name}</h1>
+        <h2 className="text-xl">Rooms available at {apartment.location}</h2>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {
+          apartment.rooms.map((room, index) => (
+            <div key={room.id}>
+              <p>Room {index + 1}</p>
+              <form>
+                <label htmlFor="name">Room name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                />
+                <label htmlFor="price">Rent price per month in € Euros:</label>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  required
+                  min={1}
+                />
+                <label htmlFor="size">Size in square meters:</label>
+                <input
+                  type="number"
+                  id="size"
+                  name="size"
+                  min={1}
+                  required
+                />
+                <label htmlFor="equipment">Equipment:</label>
+                <input
+                  className="mb-6"
+                  type="text"
+                  id="equipment"
+                  name="equipment"
+                  required
+                />
+                <button className="primary-button">Update</button>
+              </form>
+            </div>
+          ))
+        }
+      </div>
     </main>
   )
 }
