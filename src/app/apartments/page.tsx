b96effeec4295697e9246dfe511a659231a1d2c2
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-12 00:14:55 +0200

    Move getApartments back into page

--- src/app/apartments/page.tsx
+++ src/app/apartments/page.tsx
@@ -1,9 +1,38 @@
 import Link from 'next/link'
+import { createClient } from '@/utils/supabase/server'
+import { QueryData } from '@supabase/supabase-js'
+import { redirect } from 'next/navigation'
 import Image from 'next/image'
-import { getApartments } from './actions'
 
 export default async function Apartments() {
-  const apartments = await getApartments();
+  const supabase = createClient()
+
+  const { data: authData, error: authError } = await supabase.auth.getUser()
+  if (authError || !authData?.user) {
+    redirect('/login')
+  }
+
+  const apartmentsQuery = supabase
+    .from("apartments")
+    .select(`
+      id,
+      created_at,
+      name,
+      location,
+      price,
+      currency,
+      description,
+      rooms: rooms(*)
+        `
+    )
+    .order('created_at', { referencedTable: 'rooms', ascending: true })
+    .eq('user_id', authData.user.id);
+  type Apartments = QueryData<typeof apartmentsQuery>;
+
+  const { data, error } = await apartmentsQuery;
+  if (error) throw error;
+  console.log(data);
+  const apartments: Apartments = data;
 
   return (
     <main className="container mx-auto flex flex-col grow items-center">
