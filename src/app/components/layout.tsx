6427f5ef80cf37245cbfbf3db417ba3068cfc577
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-11 23:52:36 +0200

    Create separate function for handleFormSubmit

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -21,19 +21,44 @@ export function Navbar() {
 };
 
 interface BottombarProps {
-  formId: string,
-  buttonAction: (formData: FormData) => void,
+  formId: string | null,
+  buttonAction: (formData: FormData | null) => void,
   buttonTitle: string
 }
 
 export function Bottombar(props: BottombarProps) {
+  const handleFormSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const formId = props.formId;
+
+    if (formId) {
+      const form = document.getElementById(formId) as HTMLFormElement;
+      if (form.reportValidity()) {
+        const formData = new FormData(form);
+        await props.buttonAction(formData);
+      }
+    }
+  };
+
+  const handleSubmit = async () => {
+    await props.buttonAction(null);
+  };
+
   return (
     <div className="w-full bg-white px-6 py-4 flex flex-col justify-center mt-6">
       <div className="flex ml-auto">
         <Link href="/">
-          <button className="primary-button" form={props.formId} formAction={props.buttonAction}>
-            {props.buttonTitle}
-          </button>
+          {
+            props.formId ? (
+              <button className="primary-button" form={props.formId} onClick={handleFormSubmit}>
+                {props.buttonTitle}
+              </button>
+            ) : (
+              <button className="primary-button" onClick={handleSubmit}>
+                {props.buttonTitle}
+              </button>
+            )
+          }
         </Link>
       </div>
     </div>
