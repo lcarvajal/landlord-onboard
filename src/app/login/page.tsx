cfe0e3f1b922cda1cb98c35e2c2882d5358a117a
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-12 14:58:46 +0200

    Adjust colors to create dark theme

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
       <div className="md:grow text-center p-6 mt-6 md:mt-0 md:transform md:translate-y-6" >
         <h1 className="text-black md:text-white text-3xl md:text-6xl w-4/5 mx-auto font-extrabold">Earn <span className="text-orange-500">50% more</span> renting your apt to students</h1>
       </div >
-      <div className="grow flex md:items-center md:h-full md:w-4/5 lg:w-3/5 bg-white">
+      <div className="grow flex md:items-center md:h-full md:w-4/5 lg:w-3/5 bg-zinc-900">
         <form className="mx-auto">
           <h1 className="text-xl font-medium">List your apartment in 5 minutes</h1>
           <ol className="p-2 mb-6">
